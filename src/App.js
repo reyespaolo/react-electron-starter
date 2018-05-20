5d79b8b57ae6dbb4f2baec2ecef6018b34b05ed9
Paolo Reyes <reyes.paolo@360logix.com>
2018-05-20 17:22:57 +0800

    Added Radium

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
+import Radium, { StyleRoot } from 'radium'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Hello
-        </p>
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            Hello
+          </p>
+        </div>
+      </StyleRoot>
+
     );
   }
 }
 
-export default App;
+export default Radium(App);
