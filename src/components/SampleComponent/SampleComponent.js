a7e39830bd2c294e68ad5adcc6bde1d4936709e5
Paolo Reyes <reyes.paolo@360logix.com>
2018-05-21 10:26:38 +0800

    Upgrade to react 16.3

--- src/components/SampleComponent/SampleComponent.js
+++ src/components/SampleComponent/SampleComponent.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import {Button} from 'primereact/components/button/Button';
 import Aux from '../../hoc/Aux';
 //Functional Components
-const sampleComponent = (props) => {
+class sampleComponent extends Component{
 
-  return(
-    <Aux>
-      <p>Sample Component</p>
-      <Button  label={props.sampleProp}/>
-    </Aux>
-  );
+  constructor(props){
+    super(props);
+    this.inputElement = React.createRef();
+  }
+
+  componentDidMount(){
+      this.inputElement.current.focus();
+  }
+
+  render(){
+    return(
+      <Aux>
+        <p>Sample Component</p>
+        <input ref={this.inputElement}/>
+        <Button  label={this.props.sampleProp}/>
+      </Aux>
+    );
+  }
+
 }
 
 sampleComponent.propTypes = {
