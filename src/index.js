0c742996f915c6c7fd08b23508e26093bb252628
Paolo Reyes <reyes.paolo@360logix.com>
2018-05-22 14:47:37 +0800

    persistState

--- src/index.js
+++ src/index.js
@@ -1,33 +1,25 @@
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import registerServiceWorker from './config/registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import counterReducer from './store/reducers/counterReducer'
-import resultReducer from './store/reducers/resultReducer'
+import registerServiceWorker from './config/registerServiceWorker';
+import { createMemoryHistory } from 'history';
 
-const rootReducer = combineReducers({
-  counter:counterReducer,
-  result: resultReducer
-})
+import {configureStore} from './store/store'
+import App from './containers/App';
+const routerHistory = createMemoryHistory();
 
-const logger = store => {
-  return next => {
-    return action => {
-      const result = next(action);
-      console.log('Middleware', store.getState())
-      return result
-    }
+const syncHistoryWithStore = (store, history) => {
+  const { routing } = store.getState();
+  if(routing && routing.location) {
+    history.replace(routing.location);
   }
-}
+};
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)))
+const store = configureStore(routerHistory);
+syncHistoryWithStore(store, routerHistory);
 
 ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/store/store.js
@@ -0,0 +1,30 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { routerMiddleware } from 'react-router-redux';
+import persistState from 'redux-localstorage';
+
+import thunk from 'redux-thunk'
+import counterReducer from './reducers/counterReducer'
+import resultReducer from './reducers/resultReducer'
+
+export function configureStore(routerHistory) {
+  const router = routerMiddleware(routerHistory);
+  const reducers = {
+    counter:counterReducer,
+    result: resultReducer
+  };
+
+  const rootReducer = combineReducers(reducers);
+  const logger = store => {
+    return next => {
+      return action => {
+        const result = next(action);
+        console.log('Middleware', store.getState())
+        return result
+      }
+    }
+  }
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const middlewares = [ logger, thunk, router ];
+  const enhancer = composeEnhancers(applyMiddleware(...middlewares), persistState());
+  return createStore(rootReducer, enhancer);
+}
